fe8c0f979472bbed464ee2f3bd616c4b21d851b1
simsieec025 <simsieec025@student.wethinkcode.co.za>
2025-12-27 23:37:49 +0200

    planing the program

---
+++ inventory_manager.py
@@ -0,0 +1,12 @@
+import json
+import csv
+
+# i will use oop to mke thing easy for me 
+# i will have  a class inventory
+# i should have self name , self id self quantity 
+# i should have a function that will check the inventory and notify the owner if the stock if less than 5 unit
+# should have a function that ables the user to check items using an id or name
+# have a function that save the items on the json file
+# be abel to read a json file to get items from it 
+# have the condition that checks the user input
+# based on the users input then check that is valid 
---
+++ main.py
@@ -0,0 +1,3 @@
+# ask the user whether the are check or login the stock
+# if the user is login the stock the return successfully login or not
+# if the user is check the stock return the quantity, name and the id of the item in a list
